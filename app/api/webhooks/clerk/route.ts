f73e17e5ecee59f5ba28f1c1f9a9f32a3581fe2e
ake <tamiratake@gmail.com>
2024-10-18 18:31:28 +0300

    prisma stuff

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,36 @@
+// pages/api/webhooks/clerk.ts
+
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export default async function POST(req:Request, res:NextResponse) {
+
+    const { id, fullName, emailAddresses }  =  await req.json();
+
+
+    // Create or update user in your DB
+    const user = await prisma.user.upsert({
+      where: { clerkId: id },
+      update: {
+        name: fullName,
+        email: emailAddresses[0]?.emailAddress,
+      },
+      create: {
+        clerkId: id,
+        name: fullName,
+        email: emailAddresses[0]?.emailAddress,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, {status:404}) 
+    }
+ 
+   return NextResponse.json({ message: 'User synced', user }, {status:200}) 
+
+
+
+}
+
+   
